d99d0fabd69d074805c273240160f7d22df13935
vvcephei <vvceph@gmail.com>
2011-12-12 15:46:57 -0600

    init

---
+++ foo/build.sbt
@@ -0,0 +1,7 @@
+name := "foo"
+
+version := "1.0"
+
+scalaVersion := "2.9.1"
+
+
---
+++ foo/src/main/scala/org/opennlp/foo/hw.scala
@@ -0,0 +1,5 @@
+package org.opennlp.foo
+object FooHi {
+  def getSecret = 12345;
+  def main(args: Array[String]) = println("Foo Hi! "+getSecret)
+}
